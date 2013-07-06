fd7b7c4116c568a475944797a6926935337dfc6f
Mikhail Kantur <kami@slink.ws>
2013-07-06 19:22:49 +1100

    added generation of ~/.ssh/config file for heroku

--- bootstrap_local.sh
+++ bootstrap_local.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-scp -i $EC2_HOME/$EC2_KEY ${HEROKU_KEY}* ubuntu@$1:~/.ssh
+scp -i $EC2_HOME/$EC2_KEY ${HEROKU_KEY}     ubuntu@$1:~/.ssh/rsa_id
+scp -i $EC2_HOME/$EC2_KEY ${HEROKU_KEY}.pub ubuntu@$1:~/.ssh/rsa_id.pub
+
--- setup.sh
+++ setup.sh
@@ -26,6 +26,7 @@ sudo apt-add-repository -y ppa:cassou/emacs
 sudo apt-get update
 sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
 
+# make links for dotfiles
 ln -sb startup/.screenrc .
 ln -sb startup/.bash_profile .
 ln -sb startup/.bashrc .
@@ -33,4 +34,11 @@ ln -sb startup/.bashrc_custom .
 ln -sb startup/.bash_colors .
 ln -sf startup/.emacs.d .
 
+# setup heroku toolbelt
 sudo wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | bash
+
+# create ssh config file
+echo "Host heroku.com"             > ~/.ssh/config
+echo "HostName heroku.com"        >> ~/.ssh/config
+echo "IdentityFile ~/.ssh/rsa_id" >> ~/.ssh/config
+echo "IdentitiesOnly yes"         >> ~/.ssh/config
